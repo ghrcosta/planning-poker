f0803a622ced0414b569237f001c7b6be7ab2695
ghrcosta <geohrc.dev@gmail.com>
2023-07-01 08:04:11 -0300

    Spring Boot profiles, in-memory storage, GAE settings
    Implemented support for Spring Boot profiles using App Engine environment
    variable; moved in-memory storage from test to main code, to use it locally
    (instead of depending on Firebase emulator); copied more App Engine settings
    from previous project.

--- springboot-flutter/springboot/src/main/kotlin/com/ghrcosta/planningpoker/PlanningPokerApplication.kt
+++ springboot-flutter/springboot/src/main/kotlin/com/ghrcosta/planningpoker/PlanningPokerApplication.kt
@@ -2,9 +2,14 @@ package com.ghrcosta.planningpoker
 
 import com.ghrcosta.planningpoker.dependency.StorageHandler
 import com.ghrcosta.planningpoker.service.RoomService
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @SpringBootApplication
@@ -15,6 +20,29 @@ class PlanningPokerApplication {
 	}
 }
 
+/**
+ * By default, only browser requests coming from the same address ("origin") as the server are allowed. This
+ * configuration changes it so all addresses are accepted. This is required for development, as the Spring Boot server
+ * runs on port 8080 and Flutter's debug server is in a different port. The conditional property ensures this is not
+ * enabled on production.
+ */
+@Configuration
+@ConditionalOnProperty(name= ["allow-cross-origin"], havingValue="true")
+class WebConfig : WebMvcConfigurer {
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
+	}
+}
+
 fun main(args: Array<String>) {
-	runApplication<PlanningPokerApplication>(*args)
+	val log = LoggerFactory.getLogger(PlanningPokerApplication::class.java)
+
+	runApplication<PlanningPokerApplication>(*args) {
+		// https://cloud.google.com/appengine/docs/standard/java-gen2/runtime#environment_variables
+		val deployVersion = System.getenv("GAE_VERSION") ?: "local"
+		val profile = if (deployVersion == "local") "local" else "prod"
+		log.info("deploy=${deployVersion}, profile=${profile}")
+
+		this.setAdditionalProfiles(profile)
+	}
 }
